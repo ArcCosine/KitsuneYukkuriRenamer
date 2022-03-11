7c54cceb165dea329c96b877931dd06321645bfa
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-03-11 18:03:44 +0900

    [Update] Update Rename code.
    Rename.js to Rename.ts.
    Update unit test code.

--- src/__test__/rename.test.ts
+++ src/__test__/rename.test.ts
@@ -3,27 +3,26 @@ import * as fs from "fs";
 
 const beforeFile = "testfile.txt";
 const afterFile = "renamefile.txt";
-beforeAll(()=>{
+beforeAll(() => {
     try {
         const time = new Date();
         fs.utimesSync(beforeFile, time, time);
-    }catch(e){
-        fs.closeSync(fs.openSync(beforeFile, 'w'));
+    } catch (e) {
+        fs.closeSync(fs.openSync(beforeFile, "w"));
         console.log(e);
     }
-})
+});
 
-afterAll(()=>{
+afterAll(() => {
     try {
-        fs.unlinkSync(afterFile)
-    }catch(e){
-        console.log(e)
+        fs.unlinkSync(afterFile);
+    } catch (e) {
+        console.log(e);
     }
-})
+});
 
-test("rename", async ()=>{
-
+test("rename test", async () => {
     await Rename(beforeFile, afterFile);
     const isFileExsists = fs.existsSync(afterFile);
     expect(isFileExsists).toBe(true);
-})
+});
