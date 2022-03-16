48aa496e836b2f63f9b56d23b646582036fa498d
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-03-16 13:42:22 +0900

    [Remove] Remvoe one time code.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import "./Result.css";
 
+declare global { interface Window { api: any } }
+window.api = window.api || {};
+
 function App() {
     const [resultValue,setResultValue] = useState('結果をここに表示')
     const [folderPath,setFolderPath] = useState('');
 
     const handleOpenFolder = async ()=>{
         const selectFolder = await window.api.selectFolder();
-        setFolderPath(selectFolder.toString());
+        setFolderPath(selectFolder);
         setResultValue("結果をここに表示");
     };
 
     const handleTranslate = async ()=>{
-        setResultValue("変換開始");
         const results = await window.api.fileRename(folderPath);
         setResultValue("変換完了");
     };
