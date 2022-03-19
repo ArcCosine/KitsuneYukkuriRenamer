358e82188b427cc5ff3a047f9a1661bc8b44b41d
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-03-19 09:39:51 +0900

    [Update] Add bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+
 import "./App.css";
-import "./Result.css";
+import { Button } from 'react-bootstrap';
 
 declare global { interface Window { api?: any } }
 
 function App() {
-    const [resultValue,setResultValue] = useState('')
+    const [resultValue,setResultValue] = useState('結果')
     const [folderPath,setFolderPath] = useState('');
 
     const handleOpenFolder = async ()=>{
@@ -34,17 +35,27 @@ function App() {
             <header className="App-header">
                 <p>きつねゆっくりリネーマー</p>
             </header>
-            <div>
-                <input type="text" value={folderPath} id="folderPath" readOnly/>
-                <button type="button" onClick={handleOpenFolder}>フォルダを選ぶ</button>
+            <div className="container">
+                <div className="row">
+                <div className="col-md-8">
+                <input className="form-control" type="text" value={folderPath} id="folderPath" readOnly/>
                 </div>
-                <div>
-                <button type="button" onClick={handleTranslate}>変換開始</button>
+                <div className="col-md-4">
+                <Button  variant="primary" onClick={handleOpenFolder}>フォルダを選ぶ</Button>
+                </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-10">
+                <Button  variant="success" onClick={handleTranslate}>変換開始</Button>
+                </div>
+                </div>
+                <div className="row">
                 <div className="Result">{resultValue}</div>
+                </div>
             </div>
-            <div>
+            <footer>
                 <a href="https://www.flaticon.com/free-icons/tools" title="tools icons">Tools icons created by Freepik - Flaticon</a>
-            </div>
+            </footer>
         </div>
     );
 }
--- src/setting.ts
+++
@@ -1,3 +0,0 @@
-export interface ResultObject {
-    result: string;
-}
